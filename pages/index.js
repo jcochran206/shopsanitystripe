906e71f5db231a26b011c24b19175f6316273e69
jcochran206 <jonathan.o.cochran@gmail.com>
2023-01-12 21:42:15 -0800

    added components

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
+import {Product, FooterBanner, HeroBanner} from '../components'
+
 const Home = () => {
   return (
     <>
     Hero banner
 
     <div>
-      <h2>Best selling Products</h2>
+      <h2 className='products-heading'>Best selling Products</h2>
       <p>Speakers of many varitions</p>
     </div>
 
-    <div>
+    <div className='products-container'>
       {['Product 1', 'Product 2'].map((product) => product)}
     </div>
 
