97542b735d679acaf80bfc6c1d3717ff430fc390
jcochran206 <jonathan.o.cochran@gmail.com>
2023-01-12 21:55:12 -0800

    modified herobanner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Link from 'next/link';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <div className='hero-banner-container'>
+        <div>
+            <p className='beats-solo'>Small Text</p>
+            <h3>Mid Text here</h3>
+            <img src='' alt='headphones' className='hero-banner-image'/>
+            <div>
+                <Link href='/product/id'>
+                    <button type='button'>Button text</button>
+                </Link>
+                <div className='desc'>
+                    <h5>Description</h5>
+                    <p>Description</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
