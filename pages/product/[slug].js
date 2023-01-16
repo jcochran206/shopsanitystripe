1eb4e295a321b375b2084d433fd81521939627b7
jcochran206 <jonathan.o.cochran@gmail.com>
2023-01-15 22:32:04 -0800

    added thumbnails to product details page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Product from '../../components/Product';
 import {AiOutlineMinus, AiOutlinePlus,AiFillStar, AiOutlineStar } from 'react-icons/ai'
 
@@ -6,23 +6,24 @@ import {client, urlFor} from '../../lib/client';
 
 const ProductDetails = ({product, products}) => {
      const { image, name, details, price } = product;
+     const [index, setIndex] = useState(0);
 
   return (
     <div>
         <div className='product-detail-container'>
             <div>
                 <div className='image-container'>
-                  <img src={urlFor(image && image[0])} />
+                  <img src={urlFor(image && image[index])} className="product-detail-image"/>
                 </div>
-                {/* <div className='small-images-container'>
-                  {image?.map((item, i) => {
-                    <img 
+                <div className='small-images-container'>
+                  {image?.map((item, i) => (
+                    <img key={i}
                       src={urlFor(item)}
-                      className=""
-                      onMouseEnter=""
+                      className={i === index ? 'small-image selected-image' : 'small-image'}
+                      onMouseEnter={() => setIndex(i)}
                     />
-                  })}
-                </div> */}
+                  ))}
+                </div>
                 <div className='product-detail-desc'>
                   <h1>{name}</h1>
                   <div className='reviews'>
