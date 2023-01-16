7035279dd22772c773711814b7f1ffa2054af984
jcochran206 <jonathan.o.cochran@gmail.com>
2023-01-15 16:53:21 -0800

    modified css for footer banner img and added footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { AiFillInstagram, AiFillTwitterSquare } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>2023 CodingMajor All rights reserved</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiFillTwitterSquare />
+      </p>
+    </div>
   )
 }
 
