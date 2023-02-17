c3990fc9fdcf2428c73e31e06d26044686d5f039
jcochran206 <jonathan.o.cochran@gmail.com>
2023-02-16 23:01:54 -0800

    added stripe key

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,3 +1,7 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe();
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+export default async function handler(req, res){
+    
+}
