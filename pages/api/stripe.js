5fb1a2f537131ad92b64adfa3607d72941fdfcee
jcochran206 <jonathan.o.cochran@gmail.com>
2023-02-26 12:13:19 +0700

    added fetch call for stripe, modified chart, and modified stripe js to show items in console.

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -4,6 +4,7 @@ const stripe = require(Stripe)(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res){
     if (req.method === 'POST') {
+      console.log(req.body.cartItems, 'items in cart');
     try {
       const params = {
         submit_type: 'pay',
