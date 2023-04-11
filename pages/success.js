54765fa83ebede7715b1511d2b2699b750b4320a
jcochran206 <jonathan.o.cochran@gmail.com>
2023-04-10 23:06:31 -0700

    modified success component and added to context, next will work on utils file

--- pages/success.js
+++ pages/success.js
@@ -1,14 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import Link from 'next/link';
 import {BsBagCheckFill} from 'react-icons/bs';
-import { useRouter } from 'next/router';
+
 
 import {useStateContext} from '../context/StateContext';
 
 const Success = () => {
-  return (
-    <div>
+    const {setCartItems, setTotalPrice, setTotalQuantities} = useStateContext();
+    
+    useEffect(() => {
+        localStorage.clear();
+        setCartItems([]);
+        setTotalPrice(0);
+        setTotalQuantities(0);
+    }, [])
 
+  return (
+    <div className='success-wrapper'>
+        <div className='success'>
+            <p className='icon'>
+                <BsBagCheckFill />
+            </p>
+            <h2>Thank you for your order!</h2>
+            <p className='email-msg'> Check your email inbox for the receipt!</p>
+            <p className='description'>
+                If you have questions, please email
+                <a className='email' href='mailto:order@example.com'>
+                order@example.com
+                </a>
+            </p>
+            <Link href="/">
+                <button type='button' width="300px" className='btn' ></button>
+            </Link>
+        </div>
     </div>
   )
 }
